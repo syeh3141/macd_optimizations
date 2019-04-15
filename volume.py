7a19a2a2abb73a23f13b78a1b2ba99f1eaccbf4c
syeh3141 <49661930+syeh3141@users.noreply.github.com>
2019-04-15 19:32:19 -0400

    Added comments
    Need to clean up and use either of the volumeAve methods, not both!!

--- volume.py
+++ volume.py
@@ -3,12 +3,13 @@
 Created on Wed Aug  1 19:05:18 2018
 
 @author: syeh3
+Methods to compute volume
 """
 
 def volumeAve(stock):
-    
+    """Creates column in Pandas DataFrame for Volume"""
     stock['Volume SMA'] = stock['volume'].rolling(50).mean()
     
 def volumeAveHistorical(stock):
-    
+    """Creates column in Pandas DataFrame for Volume"""
     stock['Volume SMA'] = stock['Volume'].rolling(50).mean()
