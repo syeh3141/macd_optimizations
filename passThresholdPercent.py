437fe343e0073fcf1a3176e11eaac7c62296547f
syeh3141 <49661930+syeh3141@users.noreply.github.com>
2019-04-15 19:26:42 -0400

    Added comments
    Added comments to the method

--- passThresholdPercent.py
+++ passThresholdPercent.py
@@ -3,10 +3,13 @@
 Created on Sat Jan 26 18:05:09 2019
 
 @author: syeh3
+
 """
 
 def passThresholdPercent(stockData, index, days, threshold):
-    
+    """Returns True if absolute difference between start index and each day
+    up to number of total days is greater than or equal to threshold returns
+    Returns False otherwise"""
     count = 0
     
     while count <= days:
